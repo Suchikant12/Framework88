05034c23647710c557ccf41f1d919bd8235abcb4
Suchikant12 <suchi.bitu@gmail.com>
2023-08-31 00:00:42 +0530

    1st commit

---
+++ DummyPro/src/test/java/Sample/Demotest.java
@@ -0,0 +1,11 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Demotest {
+	//public static void main(String[] args) {
+	@Test
+	public void m1() {
+		System.out.println("Hi");
+	}
+}
